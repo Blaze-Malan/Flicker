478ea0fa5cb5857b25edefca740dda100df6ec7b
Blaze-Malan <nmoney.yt@gmail.com>
2026-03-31 13:37:58 +0200

    Main Button Code+Toast Button With Hint

--- app/src/main/java/za/ac/iie/flicker/MainActivity.kt
+++ app/src/main/java/za/ac/iie/flicker/MainActivity.kt
@@ -1,6 +1,12 @@
 package za.ac.iie.flicker
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -14,6 +20,50 @@ class MainActivity : AppCompatActivity() {
         installSplashScreen()
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val editText = findViewById<EditText>(R.id.editTextText)
+        //code for the button to bring us to the following pages
+        val sparkButton = findViewById<Button>(R.id.sparkButton)
+        sparkButton.setOnClickListener {
+            //helps with consistency in case someone types something "wrong" and breaks the app
+            val input = editText.text.toString().trim().lowercase()
+
+            //creating a when statement for what page the button will take the user to
+            //adding multiple options as not everyone uses midday/evening
+            when (input) {
+                "morning", "early" -> startActivity(
+                    Intent(
+                        this,
+                        MorningPage::class.java
+                    )
+                )
+
+                "afternoon", "noon", "midday" -> startActivity(
+                    Intent(
+                        this,
+                        AfternoonPage::class.java
+                    )
+                )
+
+                "evening", "late", "night" -> startActivity(
+                    Intent(
+                        this,
+                        EveningPage::class.java
+                    )
+                )
+                //adding user error handling plus an added hint for what text is acceptable
+                else -> {
+                    Toast.makeText(
+                        this, "Invalid input",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    //I couldn't find a tutorial for this so I just asked AI for help :>
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        Toast.makeText(this, "Try typing morning or evening", Toast.LENGTH_LONG)
+                            .show()
+                    }, 2000)
+                }
+            }
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
