01568bff219b7a9a2d849fe6e38b532989b6b7df
Blaze-Malan <nmoney.yt@gmail.com>
2026-03-31 12:58:36 +0200

    Splashscreen+App Icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,8 @@ android {
 }
 
 dependencies {
+    //splashscreen dependency
+    implementation("androidx.core:core-splashscreen:1.0.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
